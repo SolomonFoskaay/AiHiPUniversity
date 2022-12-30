f5c005fe0b4378b2e4ecea28bcd218fe35e204d5
Solomon Foskaay <oskfak@gmail.com>
2022-12-30 13:18:50 +0000

    7reFixed navConfig and mobilenavConfig in user/config/site.ts

--- user/config/site.ts
+++ user/config/site.ts
@@ -113,17 +113,7 @@ export const navConfig: (DD.Nav | DD.Link)[] = [
   {
     name: 'Contact',
     url: '/contact',
-  },  
-  // {
-  //   name: 'See Docs 📄',
-  //   url: 'https://docs-svelte-qwer.vercel.app/',
-  //   rel: 'external',
-  // },
-  // {
-  //   name: 'Get QWER 🚀',
-  //   url: 'https://github.com/kwchang0831/svelte-QWER',
-  //   rel: 'external',
-  // },
+  },
 ];
 
 export const mobilenavConfig: DD.Nav = {
@@ -136,16 +126,6 @@ export const mobilenavConfig: DD.Nav = {
     {
       name: 'Contact',
       url: '/contact',
-    },     
-    // {
-    //   name: 'See Docs 📄',
-    //   url: 'https://docs-svelte-qwer.vercel.app/',
-    //   rel: 'external',
-    // },
-    // {
-    //   name: 'Get QWER 🚀',
-    //   url: 'https://github.com/kwchang0831/svelte-QWER',
-    //   rel: 'external',
-    // },
+    },
   ],
 };
