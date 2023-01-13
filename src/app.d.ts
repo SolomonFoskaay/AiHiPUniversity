5a0677fed4cb470efc91bfef557725f53a146982
Solomon Foskaay <oskfak@gmail.com>
2023-01-13 19:12:21 +0000

    Removed FoskaayAi chatbot, will set it up seperate folder outside QWER

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,6 +2,13 @@
 
 declare module 'flexsearch/dist/module/index.js';
 
+// declare modules added to run Foskaayai chatbot page /FoskaayAi
+  declare module '@fortawesome/fontawesome-svg-core';
+  declare module '@fortawesome/free-solid-svg-icons';
+  declare module '@fortawesome/svelte-fontawesome';
+  declare module 'uuid';
+  declare module 'svelte-prism';
+
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 // and what to do when importing types
